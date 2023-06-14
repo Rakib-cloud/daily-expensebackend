a32c741118274e7bc9699375c896602a91ece9ae
Md .Rakibul Islam <57381896+Rakib-cloud@users.noreply.github.com>
2023-06-14 14:43:37 +0600

    deploy

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ const PORT = 4000
 app.use(express.json())
 app.use(cors())
 
-//routes
-// readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
 
 app.use("/api/v1", router)
 
